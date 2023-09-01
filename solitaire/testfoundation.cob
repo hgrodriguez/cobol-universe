ab7766fca28fb84fba224be7e4af16c005bfbc30
hrodriguez <holger.rodriguez@roseng.ch>
2023-09-01 13:32:13 +0200

    solitaire: implemented foundation as API with request/response interface

--- solitaire/testfoundation.cob
+++ solitaire/testfoundation.cob
@@ -6,128 +6,77 @@
        WORKING-STORAGE SECTION.
       
       ******************************************************************
+      * VARIABLES FOR THE TEST RUN
+       01 T-RANK-A                PIC X.
+       01 T-SUIT-A                PIC X.
+       01 TESTS-RUN               PIC 999 VALUE 0.
+       01 TESTS-OK                PIC 999 VALUE 0.
+       01 TESTS-NOK               PIC 999 VALUE 0.
+       01 TESTS-RANK              PIC 99  VALUE 1.
+
+      ******************************************************************
       * CARD DEFINITIONS
       *   DEFINES A CARD FOR THE GAME
        01 CARD.
           02 C-RANK.
-             03 RANK-A             PIC X.
-             03 RANK-N             PIC 99.
+             03 RANK-A            PIC X.
+             03 RANK-N            PIC 99.
           02 C-SUIT.
-             03 SUIT-A             PIC X.
-             03 SUIT-C             PIC X.
-             03 SUIT-N             PIC 9.
+             03 SUIT-A            PIC X.
+             03 SUIT-C            PIC X.
+             03 SUIT-N            PIC 9.
 
       ******************************************************************
-      *   THE GAME OVERALL.
-       01 GAME.
-      *      DEFINES ALL POSSIBLE CARDS IN THE GAME
-          02 CARDS.
+       01 CARDS.
       *      THE REQUEST-RESPONSE-BLOCK
-             03 REQ-RSP-BLOCK.
+          03 REQ-RSP-BLOCK.
       *            THE OPERATION REQUESTED TO BE PERFORMED
       *            1 = INITIALIZE CARDS
-                04 REQ-OP-CODE     PIC 9.
+             04 REQ-OP-CODE       PIC 9.
       *            RANK NUMBER
-                04 REQ-RANK-N      PIC 99.
+             04 REQ-RANK-N        PIC 99.
       *            SUIT NUMBER
-                04 REQ-SUIT-N      PIC 9.
+             04 REQ-SUIT-N        PIC 9.
       *            THE ERROR CODE, IF ANY, FOR THE REQUESTED OPERATION
       *            1 = ILLEGAL OP
       *            2 = ILLEGAL RANK: LOWER THAN MIN
       *            3 = ILLEGAL RANK: HIGHER THAN MAX
       *            4 = ILLEGAL SUIT: LOWER THAN MIN
       *            5 = ILLEGAL SUIT: HIGHER THAN MAX
-                04 RSP-ERR-CODE    PIC 99.
+             04 RSP-ERR-CODE      PIC 99.
       *            RANK ALPHA CODE OF REQUESTED RANK NUMBER
-                04 RSP-RANK-A      PIC X.
+             04 RSP-RANK-A        PIC X.
       *            SUIT ALPHA CODE OF REQUESTED SUIT NUMBER
-                04 RSP-SUIT-A      PIC X.
+             04 RSP-SUIT-A        PIC X.
+
+      ******************************************************************
       *      DEFINES ALL FOUNDATION STACKS OF THE GAME
-          02 FOUNDATION.
+       01 FOUNDATION.
+          03 REQ-RSP-BLOCK.
       *      THE OPERATION REQUESTED TO BE PERFORMED ON THE FOUNDATION
-             05 OP-CODE            PIC 9.
-      *      THE ERROR CODE, IF ANY, FOR THE REQUESTED OPERATION
-             05 ERR-CODE           PIC 9.
+             04 REQ-OP-CODE       PIC 9.
       *      THE SUIT OF THE CARD TO PUSH ONTO THE FOUNDATION
       *          INTO THE STACK WITH NUMBER SUIT-TO-PUSH.
-             05 SUIT-TO-PUSH       PIC 9.
+             04 REQ-SUIT-TO-PUSH  PIC 9.
+      *      THE ERROR CODE, IF ANY, FOR THE REQUESTED OPERATION
+             04 RSP-ERR-CODE      PIC 9.
       *          THE FOUNDATION HAS FOUR STACKS TO MAINTAIN
       *          THE INDEX INTO THE SPECIFIC STACK IS DEFINED BY THE
       *          SUIT NUMBER, AS WE HAVE FOUR SUITS
-             05 F-STACKS-T OCCURS 4 TIMES INDEXED BY F-STACK-I.
+          03 F-STACKS-T OCCURS 4 TIMES INDEXED BY F-STACK-I.
       *            HOW MANY CARDS ARE IN THE STACK.
-                10 COUNT-OF-CARDS  PIC 99  VALUE 0.
+             04 COUNT-OF-CARDS    PIC 99  VALUE 0.
       *            NEXT ACCEPTABLE RANK
       *            ALWAYS COUNT-OF-CARDS + 1
-                10 NEXT-RANK       PIC 99  VALUE 1.
+             04 NEXT-RANK         PIC 99  VALUE 1.
       *            SIGNAL, IF THE STACK IS FULL
-                10 IS-FULL         PIC X.
+             04 IS-FULL           PIC X.
       *            ALPHA CODE OF RANK OF TOP CARD:
       *            A,2,3,4,5,6,7,8,9,T,J,Q,K             
-                10 RANK-A          PIC X.
+             04 RANK-A            PIC X.
       *            ALPHA CODE OF SUIT OF TOP CARD:
       *            D(IAMONDS),C(LUB),H(EARTS),S(PADES)
-                10 SUIT-A          PIC X.
-      *      DEFINES THE STOCK OF THE GAME
-          02 STOCK.
-      *      THE OPERATION REQUESTED TO BE PERFORMED ON THE FOUNDATION
-             03 OP-CODE            PIC 9.
-      *      THE ERROR CODE, IF ANY, FOR THE REQUESTED OPERATION
-             03 ERR-CODE           PIC 9.
-      *      THE CARD FETCHED FROM THE STOCK
-             03 CARD-FETCHED.
-                26 RANK-N          PIC 99.
-                26 SUIT-N          PIC 9.
-      *         TOP OF STOCK PRINT REPRESENTATION
-             03 TOS-PEEK           PIC 9.
-             03 TOS-RANK-A         PIC X.
-             03 TOS-SUIT-A         PIC X.
-      *      HOW MANY CARDS ARE IN THE STOCK.
-      *      IN THE INITIALIZATION PHASE, THIS COUNTER GOES UP,
-      *        AS IT COUNTS THE CARDS TRANFERRED INTO THE STOCK
-      *      THE STOCK SHRINKS OVER TIME, WHEN WE FETCH CARDS
-             03 COUNT-OF-CARDS     PIC 99.
-      *      TABLE OF CARDS IN THE STOCK
-             03 STOCK-T OCCURS 52 TIMES INDEXED BY STOCK-I.
-                06 RANK-N          PIC 99.
-                06 SUIT-N          PIC 9.      
-      *      DEFINES ALL TABLEAU STACKS OF THE GAME
-          02 TABLEAU.
-      *      THE OPERATION REQUESTED TO BE PERFORMED ON THE TABLEAU
-             05 OP-CODE            PIC 9.
-      *      THE ERROR CODE, IF ANY, FOR THE REQUESTED OPERATION
-             05 ERR-CODE           PIC 9.
-      *         THE STACK-INDEX IN SCOPE FOR THE REQUESTED OPERATION
-             05 STACK-I-IN-SCOPE   PIC 99.
-      *         THE CARD IN SCOPE FOR THE REQUESTED OPERATION
-             05 CARD-IN-SCOPE.
-                26 RANK-N          PIC 99.
-                26 SUIT-N          PIC 9.
-      *         DATA WE NEED FOR MOVING CARDS IN THE TABLEAU
-      *         SOURCE STACK INDEX
-             05 MV-SRC-ST-I        PIC 9.
-      *         SOURCE CARD INDEX IN THE SOURCE STACK INDEX
-             05 MV-SRC-CA-I        PIC 99.
-      *         DESTINATION STACK INDEX
-             05 MV-DST-ST-I        PIC 9.
-      *         HOW MANY CARDS ARE IN THE TABLEAU.
-             05 T-COUNT-OF-CARDS   PIC 99.
-             05 T-STACKS-T OCCURS 7 TIMES INDEXED BY T-STACK-I.
-      *            HOW MANY CARDS ARE IN THE STACK.
-                10 COUNT-OF-CARDS  PIC 99  VALUE 0.
-      *            THE CARDS IN ONE STACK
-                10 CARDS-T OCCURS 52 TIMES INDEXED BY CARDS-T-I.
-                   26 RANK-N       PIC 99.
-                   26 SUIT-N       PIC 9.
-
-      ******************************************************************
-      * VARIABLES FOR THE TEST RUN
-       01 T-RANK-A                 PIC X.
-       01 T-SUIT-A                 PIC X.
-       01 TESTS-RUN                PIC 999 VALUE 0.
-       01 TESTS-OK                 PIC 999 VALUE 0.
-       01 TESTS-NOK                PIC 999 VALUE 0.
-       01 TESTS-RANK               PIC 999 VALUE 1.
+             04 SUIT-A            PIC X.
 
       ******************************************************************
        PROCEDURE DIVISION.
@@ -135,15 +84,15 @@
 
            PERFORM 01-TEST-RESET.
 
-           PERFORM VARYING SUIT-TO-PUSH
+           PERFORM VARYING REQ-SUIT-TO-PUSH
               FROM 1 BY 1
-              UNTIL SUIT-TO-PUSH > 4
+              UNTIL REQ-SUIT-TO-PUSH > 4
                    PERFORM 02-TEST-SUIT-PUSH-ALL-OK
            END-PERFORM
 
-           PERFORM VARYING SUIT-TO-PUSH
+           PERFORM VARYING REQ-SUIT-TO-PUSH
               FROM 1 BY 1
-              UNTIL SUIT-TO-PUSH > 4
+              UNTIL REQ-SUIT-TO-PUSH > 4
                    PERFORM 03-TEST-SUIT-PUSH-1-TO-MANY
            END-PERFORM
 
@@ -158,7 +107,7 @@
            PERFORM 01-FOUNDATION-RESET.
 
            ADD 1 TO TESTS-RUN.
-           IF ERR-CODE OF FOUNDATION IS EQUAL TO 0
+           IF RSP-ERR-CODE OF FOUNDATION IS EQUAL TO 0
               ADD 1 TO TESTS-OK
            ELSE
               ADD 1 TO TESTS-NOK
@@ -214,17 +163,17 @@
        02-TEST-SUIT-PUSH-ALL-OK.
            PERFORM 01-FOUNDATION-RESET.
 
-           MOVE SUIT-TO-PUSH TO F-STACK-I.
+           MOVE REQ-SUIT-TO-PUSH TO F-STACK-I.
 
            PERFORM VARYING TESTS-RANK
               FROM 1 BY 1
               UNTIL TESTS-RANK > 13
-                   MOVE 2 TO OP-CODE OF FOUNDATION
-                   CALL 'FOUNDATION' USING GAME
+                   MOVE 2 TO REQ-OP-CODE OF FOUNDATION
+                   CALL 'FOUNDATION' USING REQ-RSP-BLOCK OF FOUNDATION
                    END-CALL
 
                    ADD 1 TO TESTS-RUN
-                   IF ERR-CODE OF FOUNDATION IS EQUAL TO 0
+                   IF RSP-ERR-CODE OF FOUNDATION IS EQUAL TO 0
                       ADD 1 TO TESTS-OK
                    ELSE
                       ADD 1 TO TESTS-NOK
@@ -274,24 +223,24 @@
        03-TEST-SUIT-PUSH-1-TO-MANY.
            PERFORM 01-FOUNDATION-RESET.
 
-           MOVE SUIT-TO-PUSH TO F-STACK-I.
+           MOVE REQ-SUIT-TO-PUSH TO F-STACK-I.
 
            PERFORM VARYING TESTS-RANK
               FROM 1 BY 1
               UNTIL TESTS-RANK > 13
-                   MOVE 2 TO OP-CODE OF FOUNDATION
-                   CALL 'FOUNDATION' USING GAME
+                   MOVE 2 TO REQ-OP-CODE OF FOUNDATION
+                   CALL 'FOUNDATION' USING REQ-RSP-BLOCK OF FOUNDATION
                    END-CALL
            END-PERFORM.
-           MOVE 2 TO OP-CODE OF FOUNDATION
-           CALL 'FOUNDATION' USING GAME
+           MOVE 2 TO REQ-OP-CODE OF FOUNDATION
+           CALL 'FOUNDATION' USING REQ-RSP-BLOCK OF FOUNDATION
            END-CALL.
            ADD 1 TO TESTS-RUN
-           IF ERR-CODE OF FOUNDATION IS EQUAL TO 1
+           IF RSP-ERR-CODE OF FOUNDATION IS EQUAL TO 1
               ADD 1 TO TESTS-OK
            ELSE
               ADD 1 TO TESTS-NOK
-              DISPLAY "ERR-CODE=" ERR-CODE OF FOUNDATION
+              DISPLAY "ERR-CODE=" RSP-ERR-CODE OF FOUNDATION
            END-IF.
 
       ******************************************************************
@@ -301,16 +250,17 @@
            PERFORM VARYING F-STACK-I
               FROM 1 BY 1
               UNTIL F-STACK-I > 4
-                   MOVE F-STACK-I TO SUIT-TO-PUSH
+                   MOVE F-STACK-I TO REQ-SUIT-TO-PUSH
                    PERFORM VARYING TESTS-RANK
                       FROM 1 BY 1
                       UNTIL TESTS-RANK > 13
-                           MOVE 2 TO OP-CODE OF FOUNDATION
-                           CALL 'FOUNDATION' USING GAME
+                           MOVE 2 TO REQ-OP-CODE OF FOUNDATION
+                           CALL 'FOUNDATION' USING REQ-RSP-BLOCK
+                              OF FOUNDATION
                            END-CALL
 
                            ADD 1 TO TESTS-RUN
-                           IF ERR-CODE OF FOUNDATION IS EQUAL TO 0
+                           IF RSP-ERR-CODE OF FOUNDATION IS EQUAL TO 0
                               ADD 1 TO TESTS-OK
                            ELSE
                               ADD 1 TO TESTS-NOK
@@ -324,7 +274,7 @@
                            MOVE TESTS-RANK TO REQ-RANK-N OF CARDS
                            MOVE F-STACK-I TO REQ-SUIT-N OF CARDS 
                            MOVE 2 TO REQ-OP-CODE OF CARDS
-                           CALL 'CARDS' USING GAME
+                           CALL 'CARDS' USING REQ-RSP-BLOCK OF CARDS
                            END-CALL
 
                            MOVE RSP-RANK-A OF CARDS TO RANK-A OF CARD 
@@ -351,6 +301,7 @@
       *                    CHECK SUIT
                            MOVE SUIT-A OF F-STACKS-T(F-STACK-I) TO
                               T-SUIT-A
+                           ADD 1 TO TESTS-RUN
                            IF SUIT-A OF C-SUIT IS EQUAL TO T-SUIT-A
                               ADD 1 TO TESTS-OK
                            ELSE
@@ -371,9 +322,9 @@
       ******************************************************************
        01-FOUNDATION-RESET.
            MOVE 1 TO REQ-OP-CODE OF CARDS.
-           CALL 'CARDS' USING GAME
+           CALL 'CARDS' USING REQ-RSP-BLOCK OF CARDS
            END-CALL.
 
-           MOVE 1 TO OP-CODE OF FOUNDATION.
-           CALL 'FOUNDATION' USING GAME
+           MOVE 1 TO REQ-OP-CODE OF FOUNDATION.
+           CALL 'FOUNDATION' USING REQ-RSP-BLOCK OF FOUNDATION
            END-CALL.
